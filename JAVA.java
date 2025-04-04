202b7327c5e9506a3e35ffa64e4459d7689460ee
kavya12378 <01fe23bca142@gmail.com>
2025-04-04 08:26:20 +0530

    save file

---
+++ JAVA.java
@@ -0,0 +1,13 @@
+package java;
+
+public class JAVA {
+    public static void main(String[] args) {
+        int a=10;
+        int b=3;
+        System.out.println("a+b=" + (a+b));
+        System.out.println("a-b=" + (a-b));
+        System.out.println("a/b=" + (a/b));
+
+    }
+    
+}

4815d1b0293d858a083a1634ab336e3d6cb99a8d
kavya12378 <01fe23bca142@gmail.com>
2025-04-04 09:10:09 +0530

    save

---
+++ third.java
@@ -0,0 +1,10 @@
+package java;
+
+public class third {
+    public static void main(String[] args) {
+        int a=10;
+        int b=3;
+        System.out.println("a%b=" + (a%b));
+        
+    }
+}
